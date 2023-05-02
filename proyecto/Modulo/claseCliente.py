4d8c350dfc69c752767f86f7dea212fdf3d337e8
codingwthisa <106448788+codingwthisa@users.noreply.github.com>
2023-05-02 11:19:15 -0500

    Update claseCliente.py

--- proyecto/Modulo/claseCliente.py
+++ proyecto/Modulo/claseCliente.py
@@ -10,12 +10,4 @@ class Cliente:
         total_factura = factura.calcular_total_factura()
         self.facturas[-1].total_factura = total_factura
 
-    def ver_historial(self):
-        for factura in self.facturas:
-            print(f"Fecha de compra: {factura.fecha}")
-            print("Productos:")
-            for producto in factura.productos:
-                print(f"- {producto.nombre}: {producto.valor}")
-            print(f"Total: {factura.total_factura}")
-            print()
-
+    
